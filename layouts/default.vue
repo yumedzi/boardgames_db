f2204a19d637f0bf1bd816c261d65e735a61d733
Viktor Moyseyenko <vityok@gmail.com>
2020-05-15 22:24:00 +0300

    Fixing Mobile view - issue with drawer not working

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,23 +20,25 @@
         </v-list>
       </v-navigation-drawer>
     </client-only>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <client-only>
+      <v-app-bar :clipped-left="clipped" fixed app>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <v-btn icon @click.stop="miniVariant = !miniVariant">
+          <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        </v-btn>
+        <v-btn icon @click.stop="clipped = !clipped">
+          <v-icon>mdi-application</v-icon>
+        </v-btn>
+        <v-btn icon @click.stop="fixed = !fixed">
+          <v-icon>mdi-minus</v-icon>
+        </v-btn>
+        <v-toolbar-title v-text="title" />
+        <v-spacer />
+        <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </client-only>
     <v-content>
       <v-container>
         <nuxt />
