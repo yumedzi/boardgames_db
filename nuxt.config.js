a052017ada118136f276bdf7bb2c171c2a9a1da8
Viktor Moyseyenko <vityok@gmail.com>
2020-05-23 22:26:27 +0300

    Added nuxtClientInit fetch actions

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,6 +36,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/nuxt-client-init.client.js'
   ],
   /*
   ** Nuxt.js dev-modules
--- store/index.js
+++ store/index.js
@@ -1,7 +1,14 @@
 export const actions = {
   async nuxtServerInit({ state, commit, dispatch }, { req }) {
+    // console.log("Passing collected data to client...");
+    // await dispatch('games/fetchGenres')
+    // await dispatch('games/fetchGames')
+  },
+  async nuxtClientInit({ state, commit, dispatch }, { req }) {
     // console.log(req)
-    console.log("Passing collected data to client...");
+    console.log("Running INIT calls on client...");
+    console.log(localStorage.getItem('useDarkTheme') || 'false');
+
     // commit('games/SET_GAMES', state.games.games)
     // commit('games/SET_GENRES', state.games.genres)
     await dispatch('games/fetchGenres')
